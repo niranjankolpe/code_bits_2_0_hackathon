a16034fbfdd08c6705ba9171d67a1680a1446b0f
niranjankolpe <en20170973@git-india.edu.in>
2024-04-05 19:52:54 +0530

    Built URL API and Database Models

---
+++ recommender/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from recommender.models import *
+
+# Register your models here.
+admin.site.register(AcademicPerformance)
+admin.site.register(StudentGoals)
+admin.site.register(StudentProfiles)
+admin.site.register(TrialModel)
